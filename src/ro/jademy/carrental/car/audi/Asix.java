dca25cb70cd29f08b7b2e9f737396f7bd0f7bc10
Dumitrica Dan <dumitricadan@yahoo.com>
2018-10-03 19:58:47 +0300

    Car rental v3 - classes&packages

--- src/ro/jademy/carrental/car/audi/Asix.java
+++ src/ro/jademy/carrental/car/audi/Asix.java
@@ -1,9 +1,11 @@
 package ro.jademy.carrental.car.audi;
 
 import com.sun.xml.internal.ws.api.pipe.Engine;
+import ro.jademy.carrental.car.CarType;
+import ro.jademy.carrental.car.TransmissionType;
 
 public class Asix extends Audi {
-    public Asix(Integer year, String carType, Integer doorNumber, String color, String transmissionType, Engine engine, String basePrice) {
-        super("Asix", year, carType, doorNumber, color, transmissionType, engine, basePrice);
+    public Asix(Integer year, CarType carType, Integer doorNumber, String color, TransmissionType transmissionType, Engine engine, String basePrice) {
+        super("Asix", year, CarType.SEDAN, doorNumber, color, TransmissionType.AUTOMATIC, engine, basePrice);
     }
 }
